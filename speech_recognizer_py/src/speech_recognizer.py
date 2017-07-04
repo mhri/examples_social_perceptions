73a20f3b9627c2387edb213ef5fbd59da13be07a
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2017-07-04 14:36:04 +0900

    Add start/stop recognition

--- speech_recognizer_py/src/speech_recognizer.py
+++ speech_recognizer_py/src/speech_recognizer.py
@@ -4,7 +4,7 @@
 import rospy
 from perception_base.perception_base import PerceptionBase
 
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Empty, Bool
 from mhri_social_msgs.msg import RecognizedWord
 
 
@@ -14,6 +14,8 @@ class SpeechRecognizer(PerceptionBase):
 
         rospy.Subscriber('recognized_word', RecognizedWord, self.handle_recognized_word)
         rospy.loginfo('[%s] initialze done...'%rospy.get_name())
+
+        self.pub_enable_google_speech = rospy.Publisher('enable_recognition', Bool, queue_size=5)
         rospy.spin()
 
     def handle_recognized_word(self, msg):
@@ -24,6 +26,15 @@ class SpeechRecognizer(PerceptionBase):
 
         self.save_to_memory(self.conf_data.keys()[0], data=write_data)
         self.raise_event(self.conf_data.keys()[0], 'speech_recognized')
+
+    def handle_start_perception(self, msg):
+        self.pub_enable_google_speech.publish(True)
+        super(SpeechRecognizer, self).handle_start_perception(msg)
+
+
+    def handle_stop_perception(self, msg):
+        self.pub_enable_google_speech.publish(False)
+        super(SpeechRecognizer, self).handle_stop_perception(msg)
 
 
 if __name__ == '__main__':
